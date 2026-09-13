9a21b11a97b825af6528ec083db2cc54d591d9cf
zackrelton20-ctrl <zackrelton20@hotmail.com>
2026-09-13 13:52:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MOFOMONPLAN
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/zackrelton20-ctrl/MOFOMONPLAN)
---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "money-plan-v1";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon.svg"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES))
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(cached => cached || fetch(event.request))
+  );
+});
