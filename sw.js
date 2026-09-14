8d7828775916f1b4702e804a35ea5fb335dbb9dc
zackrelton20-ctrl <zackrelton20@hotmail.com>
2026-09-14 12:25:49 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "money-plan-v3";
+const CACHE_NAME = "money-plan-v4";
 
 const FILES = [
   "./",
@@ -8,32 +8,98 @@ const FILES = [
   "./android-chrome-512x512.png"
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(FILES))
-  );
 
-  self.skipWaiting();
-});
+self.addEventListener(
+  "install",
+  event => {
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
-    )
-  );
+    event.waitUntil(
 
-  self.clients.claim();
-});
+      caches
+        .open(CACHE_NAME)
+        .then(
+          cache =>
+            cache.addAll(FILES)
+        )
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(cached => cached || fetch(event.request))
-  );
-});
+    );
+
+    self.skipWaiting();
+
+  }
+);
+
+
+self.addEventListener(
+  "activate",
+  event => {
+
+    event.waitUntil(
+
+      caches
+        .keys()
+        .then(
+          keys =>
+
+            Promise.all(
+
+              keys
+                .filter(
+                  key =>
+                    key !== CACHE_NAME
+                )
+                .map(
+                  key =>
+                    caches.delete(key)
+                )
+
+            )
+
+        )
+
+    );
+
+    self.clients.claim();
+
+  }
+);
+
+
+self.addEventListener(
+  "fetch",
+  event => {
+
+    event.respondWith(
+
+      fetch(event.request)
+
+        .then(response => {
+
+          const copy =
+            response.clone();
+
+          caches
+            .open(CACHE_NAME)
+            .then(
+              cache =>
+                cache.put(
+                  event.request,
+                  copy
+                )
+            );
+
+          return response;
+
+        })
+
+        .catch(
+          () =>
+            caches.match(
+              event.request
+            )
+        )
+
+    );
+
+  }
+);
